946311e77d33d584e6fcfd9f87d0199242973947
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-04-12 16:26:36 +0200

    Fix variadic args in new err function

--- src/util.h
+++ src/util.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 static inline void err(int status, char *format, ...) {
-    va_list ap;
-    fprintf(stderr, format, ap);
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+   
     exit(EXIT_FAILURE);
 }
